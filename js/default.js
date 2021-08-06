02084c289870d314af2ec734192da2f9d1c19738
Julius Ayotunde Idowu <punchyglobalmedia@gmail.com>
2021-08-06 16:08:24 +0100

    Julius Ayotunde Idowu Portfolio
    My First Project

---
+++ js/default.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    alert('Welcome to Punchy Global Media');    
+    $(".button").click(function(){
+    $(".lgd").toggle(3000);
+    });
+    $(".button2").click(function(){
+    $(".myForm").fadeToggle(3000);
+    });
+    $(".button3").click(function(){
+    $(".aboutMe").toggle("slow");
+    });
+    $(".sub1").click(function(){
+    $(".sub").toggle("slow");
+    }); 
+    
+});
+  
+
+ 
+  
+ 
